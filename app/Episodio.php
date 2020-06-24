1db9a54120e01e8a298c61111ba0559637af90fd
Gabriel <gabrielvieira840@gmail.com>
2020-06-24 19:48:29 -0300

    Todas implentações colocadas com autenticação de usuario

--- app/Episodio.php
+++ app/Episodio.php
@@ -9,10 +9,25 @@ use Illuminate\Database\Eloquent\Model;
 class Episodio extends Model
 {
     public $timestamps = false;
-    protected $fillable = ['temporada', 'numero', 'assistido'];
+    protected $fillable = ['temporada', 'numero', 'assistido', 'serie_id'];
+    protected $appends = ['links'];
+    protected $perPage = 5;
 
     public function serie()
     {
         return $this->belongsTo(Serie::class);
+    }
+
+    public function getAssistidoAttribute($assistido) : bool
+    {
+        return $assistido;
+    }
+
+    public function getLinksAttribute() : array
+    {
+        return [
+            'self' => '/api/episodios/' . $this->id,
+            'series' => '/api/series/' . $this->serie_id
+        ];
     }
 }
--- app/Serie.php
+++ app/Serie.php
@@ -9,8 +9,19 @@ class Serie extends Model
 {
     public $timestamps = false;
     public $fillable = ['nome'];
+    protected $appends = ['links'];
+    protected $perPage = 5;
+
     public function episodios()
     {
         return $this->hasMany(Episodio::class);
+    }
+
+    public function getLinksAttribute() : array
+    {
+        return [
+            'self' => '/api/series/' . $this->id,
+            'episodios' => '/api/series/' . $this->id . '/episodios'
+        ];
     }
 }
